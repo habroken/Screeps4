a61348bd9b2e98dbbe0cf892fd6a9f3733231ab2
habroken <dejanpieter@gmail.com>
2017-04-09 19:30:57 +0200

    testvc

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// test blablabla
+var roleHarvester = require('role.harvester');
+
+module.exports.loop = function () {
+    // for every creep name in Game.creeps
+    for (let name in Game.creeps)   {
+        // get the creep object
+        var creep = Game.creeps[name];
+
+        roleHarvester.run(creep);
+    }
+}
